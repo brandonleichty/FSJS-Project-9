b7c2981011f8642a563bbbd680984b64726eb244
Brandon Leichty <brandonl@me.com>
2017-07-09 19:07:12 -0500

    added addRecipe() function to $scope

--- public/scripts/controllers/recipeController.controller.js
+++ public/scripts/controllers/recipeController.controller.js
@@ -5,11 +5,11 @@
         .module('app')
         .controller('RecipesController', RecipesController);
 
-    function RecipesController($scope, $http, $location, $routeParams, dataService) {
+    function RecipesController($scope, $location, $routeParams, dataService) {
         
     
 
-        // Get complete list of categories
+        // Get complete list of categories and assign the data to $scope.categories
         dataService.getCategories(function (response) {
             $scope.categories = response.data;
         });
@@ -22,6 +22,11 @@
 		$scope.delete = function(recipe_id, index) {
             dataService.deleteRecipe(recipe_id);
             $scope.recipes.splice(index, 1);
+        }
+
+
+        $scope.addRecipe = function() {
+            $location.path('/add');
         }
 	}
     
