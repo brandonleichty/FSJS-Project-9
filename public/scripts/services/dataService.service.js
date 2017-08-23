e1842844028498f112181f6b668c9f3e0b6d01d8
Brandon Leichty <brandonleichty@Brandons-MacBook-Pro.local>
2017-08-23 17:18:40 -0500

    modified dataServices function

--- public/scripts/services/dataService.service.js
+++ public/scripts/services/dataService.service.js
@@ -10,48 +10,51 @@ function dataService($http, $location, $routeParams) {
 
     // GET /api/recipes - gets all of the recipes
     this.getRecipes = function (callback) {
-        $http.get('/api/recipes').then(successCallback, errorCallback);
-    };
+        $http.get('/api/recipes').then(callback,errorCallback);
+        return callback.data;
+    }
 
     // GET /api/categories - gets all of the categories
     this.getCategories = function (callback) {
-        $http.get('/api/categories').then(successCallback, errorCallback);
+        $http.get('/api/categories').then(callback, errorCallback);
     };
 
     // GET /api/fooditems - gets all of the food items
     this.getFoodItems = function (callback) {
-        $http.get('/api/fooditems').then(successCallback, errorCallback);
+        $http.get('/api/fooditems').then(callback, errorCallback);
     };
 
     // GET /api/recipes?category={category}
     // Gets all of the recipes for the specified category
     this.getRecipesByCategory = function (category, callback) {
-        $http.get('/api/recipes?category=' + category).then(successCallback, errorCallback);
+        $http.get('/api/recipes?category=' + category).then(callback, errorCallback);
     };
 
     // GET /api/recipes/{id} - Gets the recipe for the specified ID
     this.getRecipe = function (recipeId, callback) {
-        $http.get('/api/recipes/' + recipeId).then(callback);
+        $http.get('/api/recipes/' + recipeId).then(callback, errorCallback);
 
     }
 
     // POST /api/recipes - Adds a recipe
     this.addRecipe = function (recipe, callback) {
-        $http.post('/api/fooditems', recipe).then(successCallback, errorCallback);
-
+        $http.post('/api/recipes', recipe).then(callback, errorCallback);
     }
 
     // PUT /api/recipes/{id} - dpdates the recipe for the specified ID
     this.updateRecipe = function (recipe, callback) {
-        $http.put('/api/recipes/' + recipe._id, recipe).then(successCallback, errorCallback);
-
+        $http.put('/api/recipes/' + recipe._id, recipe).then(callback);
     }
 
     // DELETE /api/recipes/{id} - deletes the recipe for the specified ID
     this.deleteRecipe = function (recipeId, callback) {
-        $http.delete('/api/recipes/' + recipeId).then(successCallback, errorCallback);
+        $http.delete('/api/recipes/' + recipeId).then(callback, errorCallback);
 
     }
+}
+
+function errorCallback(error) {
+    console.log(error);
 }
 
 })();
